76b9b3be935113b0a5ecd2df525130df6967acfe
gmp.vinbrown <gmp.vinbrown@gmail.com>
2018-05-20 21:06:42 -0400

    add IFinancialService and additional attendance methods

--- gmp.services.contracts/IAttendanceService.cs
+++ gmp.services.contracts/IAttendanceService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using gmp.DomainModels.Entities;
 
 namespace gmp.services.contracts
 {
@@ -7,5 +9,7 @@ namespace gmp.services.contracts
         int AddAttendance(int memberId, int eventId, DateTime eventDate, string notes);
         int AddRegistration(int memberId, int eventActivityId, int? paymentId);
         void UpdateRegistration(int registrationId, int eventId, int? paymentId);
+        Event GetEvent(int eventId);
+        List<EventActivityType> GetEventActivityTypes();
     }
 }
---
+++ gmp.services.contracts/IFinancialService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gmp.services.contracts
+{
+    public interface IFinancialService
+    {
+    }
+}
